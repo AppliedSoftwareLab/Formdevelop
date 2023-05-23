802fc6e392a08a44749735d5f2955ddf6745d46f
junwon <apro500@naver.com>
2023-05-23 21:39:04 +0900

    pictureBox를 초기화 하는 함수 추가

--- Form1.cs
+++ Form1.cs
@@ -27,5 +27,19 @@ namespace DBViewer
         {
 
         }
-
+        private void InitializePictureBox()
+        {
+            pictureBox = new PictureBox();
+            pictureBox.Location = new Point(0, 0);
+            pictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox.ImageLocation = "bird.gif";
+            pictureBox.LoadCompleted += PictureBox_LoadCompleted;
+            Controls.Add(pictureBox);
+        }
+        private void PictureBox_LoadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            pictureBox.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            pictureBox.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+        }
+    }
 }
