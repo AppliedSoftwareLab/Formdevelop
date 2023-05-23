ce561dbbb107ff70bf6068363092a4c39ef8fbc2
junwon <apro500@naver.com>
2023-05-23 23:03:57 +0900

    폰트아틀라스 실행시 성공 실패에 대한 메세지 박스 추가

--- Form1.cs
+++ Form1.cs
@@ -104,6 +104,16 @@ namespace DBViewer
             // Process 실행
             process2.Start();
             process2.WaitForExit();
+
+            // 성공적으로 실행되었을 경우
+            if (process2.ExitCode == 0)
+            {
+                MessageBox.Show("GenerateprojectFiles.bat 실행이 완료되었습니다.");
+            }
+            else
+            {
+                MessageBox.Show("GenerateprojectFiles.bat 실행 중 오류가 발생했습니다.");
+            }
         }
     }
 }
