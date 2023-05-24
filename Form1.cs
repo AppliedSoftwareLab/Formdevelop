500c33c0d60959cb3ec18b84c0661dfaea5602ce
junwon <apro500@naver.com>
2023-05-24 09:32:05 +0900

    성공적으로 실행 됐을때의 메시지 박스를 보여주도록함

--- Form1.cs
+++ Form1.cs
@@ -113,7 +113,21 @@ namespace DBViewer
                 process2.StandardInput.WriteLine($"cd {workingDirectory}");
                 // "GenerateProjectFiles.bat" 명령 실행
                 process2.StandardInput.WriteLine($"{batFilePath} {arguments}");
+                process2.StandardInput.Flush();
+                process2.StandardInput.Close();
 
+                process2.WaitForExit();
+                // 성공적으로 실행되었을 경우
+                if (process2.ExitCode == 0)
+                {
+                    MessageBox.Show("GenerateprojectFiles.bat 실행이 완료되었습니다.");
+                }
+                else
+                {
+                    MessageBox.Show("GenerateprojectFiles.bat 실행 중 오류가 발생했습니다.");
+                }
+            }
+     
 
 
         }
