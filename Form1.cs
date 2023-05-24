6ec2dd81001929abfc5a07aaaedc6a34b12f7608
junwon <apro500@naver.com>
2023-05-24 09:43:50 +0900

    성공 실패시 메시지 박스 추가

--- Form1.cs
+++ Form1.cs
@@ -84,6 +84,17 @@ namespace DBViewer
                 process1.StandardInput.Flush();
                 process1.StandardInput.Close();
 
+                process1.WaitForExit();
+                // 성공적으로 실행되었을 경우
+                if (process1.ExitCode == 0)
+                {
+                    MessageBox.Show("GenerateprojectFiles.bat 실행이 완료되었습니다.");
+                }
+                else
+                {
+                    MessageBox.Show("GenerateprojectFiles.bat 실행 중 오류가 발생했습니다.");
+                }
+
             }
             catch (Exception ex)
             {
